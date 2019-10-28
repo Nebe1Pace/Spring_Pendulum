35bdf946105f33446febf4731b673e502cff8973
Nikita <45326590+Nebe1Pace@users.noreply.github.com>
2019-10-28 14:39:43 +0500

    git add

---
+++ Spring_Pendulum/Assets/scripts/sphere_move.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sphere_move : MonoBehaviour {
+
+    void Update()
+    {
+        transform.Translate(new Vector3(0,Mathf.Sin(Time.realtimeSinceStartup / 10 * Mathf.PI * 2f), 0));
+    }
+}
